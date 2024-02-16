53fbd396191e2441e5d3e34a29a1ca60df6f4423
today-is-first <diligent0644@gmail.com>
2024-02-17 03:45:18 +0900

    feat: Header 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
 import WeekCard from './components/WeekCard';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className='App'>
+    <div className="App">
+      <Header />
       <WeekCard />
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import Logo from '../../public/asset/Logo.svg';
+import menu from '../../public/asset/menu.svg';
+import person from '../../public/asset/person_outline.svg';
+const Header = () => {
+  return (
+    <section className="header">
+      <div className="logo-wrapper">
+        <img src={Logo} className="logo" />
+      </div>
+      <div className="navbar">
+        <a>이벤트</a>
+        <a>고객센터</a>
+        <div className="profile-wrapper">
+          <div className="hamburger">
+            <img src={menu} />
+          </div>
+          <div className="profile">
+            <img src={person} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
